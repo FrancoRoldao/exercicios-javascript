b6ca471481b929e798c06d64509304529e38c16e
Franco Roldão <francotrabalhoestudo@gmail.com>
2023-04-05 11:14:02 -0300

    exercicio Array e loop
    exercicio que insere todos os numeros pares no intervalo de 10 a 50 em uma lista e os imprime.

---
+++ ImprimenumerosParesIntervalo10A50.js
@@ -0,0 +1,16 @@
+// Faça um programa que preencha uma lista com todos os números pares contidos no intervalo de 10 a 50;
+
+let lista = [];
+
+for (let i = 10; i <= 50; i++) {
+
+
+    if (i % 2 == 0) {
+
+        lista.push(i);
+
+    }
+
+}
+
+console.log("Números Pares no intervalo de 10 a 50: " + lista);
